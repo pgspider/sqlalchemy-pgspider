a24cc37e791e24a1785a122e6463fd2ea156f8b8
m-aikawa <megumi.aikawa@toshiba.co.jp>
2023-03-01 17:38:39 +0900

    add sample code

---
+++ sqlalchemy_pgspider/psycopg2.py
@@ -0,0 +1,20 @@
+import re
+
+from sqlalchemy.dialects.postgresql.psycopg2 import PGDialect_psycopg2
+
+
+class PGSpiderDialect_psycopg2(PGDialect_psycopg2):
+    supports_statement_cache=True
+    
+    def _get_server_version_info(self, connection):
+        v = connection.exec_driver_sql("select pg_catalog.version()").scalar()
+        m = re.match(
+            r".*(?:PGSpider) "
+            r"(\d+)\.?(\d+)?(?:\.(\d+))?(?:\.\d+)?(?:devel|beta)?",
+            v,
+        )
+        if not m:
+            raise AssertionError(
+                "Could not determine version from string '%s'" % v
+            )
+        return tuple([int(x) for x in m.group(1, 2, 3) if x is not None])
---
+++ test.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine, inspect, text
+from sqlalchemy.dialects import registry as _registry
+
+import sqlalchemy_pgspider
+
+_registry.register(
+    "pgspider.psycopg2", "sqlalchemy_pgspider.psycopg2", "PGSpiderDialect_psycopg2"
+)
+
+engine = create_engine("pgspider+psycopg2://pgspider:pgspider@/pgspiderdb?host=localhost&port=48131")
+insp = inspect(engine)
+
+with engine.connect() as conn:
+    # conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),[{"x": 1, "y": 1}, {"x": 2, "y": 4}])
