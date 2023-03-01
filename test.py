6b99eb1ea59a99608ec48bd2bb2a55cd4bdd4bce
m-aikawa <megumi.aikawa@toshiba.co.jp>
2023-03-01 18:24:36 +0900

    remove comment out on create table.

--- test.py
+++ test.py
@@ -11,5 +11,5 @@ engine = create_engine("pgspider+psycopg2://pgspider:pgspider@/pgspiderdb?host=l
 insp = inspect(engine)
 
 with engine.connect() as conn:
-    # conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
     conn.execute(text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),[{"x": 1, "y": 1}, {"x": 2, "y": 4}])
