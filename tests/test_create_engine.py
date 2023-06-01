d6c2b617b01a999337c82d77b588254b73324916
m-aikawa <megumi.aikawa@toshiba.co.jp>
2023-06-01 13:47:12 +0900

    Change to use URL.create() to generate test URLs

--- tests/test_create_engine.py
+++ tests/test_create_engine.py
@@ -1,27 +1,39 @@
 import pytest
 from sqlalchemy import create_engine, inspect
+from sqlalchemy.engine.url import URL
 
+
+def url(drivername, settings):
+    return URL.create(
+        drivername = drivername,
+        username = settings['DBUSER'],
+        password = settings['DBPASS'],
+        host = settings['DBHOST'],
+        port = settings['DBPORT'],
+        database = settings['DBNAME'],
+    )
 
 @pytest.mark.parametrize(
-    "pgspider_protocol",
+    "pgspider_drivername",
     [
         "pgspider",
         "pgspider+psycopg2",
     ]
 )
-def test_create_engine(settings, pgspider_protocol):
-    engine = create_engine(f"{pgspider_protocol}://{settings['DBUSER']}:{settings['DBPASS']}@/{settings['DBNAME']}?host={settings['DBHOST']}&port={settings['DBPORT']}")
+def test_create_engine(settings, pgspider_drivername):
+    engine = create_engine(url(pgspider_drivername, settings))
+    print(url(pgspider_drivername, settings))
     inspect(engine)
 
 @pytest.mark.parametrize(
-    "otherdb_protocol",
+    "otherdb_drivername",
     [
         "postgresql",
         "postgresql+psycopg2",
     ]
 )
-def test_create_engine_with_other_protocol(settings, otherdb_protocol):
-    engine = create_engine(f"{otherdb_protocol}://{settings['DBUSER']}:{settings['DBPASS']}@/{settings['DBNAME']}?host={settings['DBHOST']}&port={settings['DBPORT']}")
+def test_create_engine_with_other_protocol(settings, otherdb_drivername):
+    engine = create_engine(url(otherdb_drivername, settings))
     with pytest.raises(Exception) as e:
         inspect(engine)
     assert "Could not determine version from string" in str(e.value)
